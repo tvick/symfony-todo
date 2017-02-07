7ca549b24cd210c4b2cf18214390d3a3b258030e
Tim Vick <t.vickc@gmail.com>
2017-02-06 22:47:10 -0800

    added task and todolist repositories

--- src/AppBundle/Entity/Task.php
+++ src/AppBundle/Entity/Task.php
@@ -8,7 +8,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * Individual item held in the Todo list
  *
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TaskRepository")
  * @ORM\Table(name="task")
  */
 class Task
